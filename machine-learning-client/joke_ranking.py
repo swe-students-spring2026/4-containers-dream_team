8c0aaf2d151f5430a123eec7df8a39f5177f9621
nuzhny25 <sashanuzhny@gmail.com>
2026-04-10 13:30:36 -0400

    black formating

--- machine-learning-client/joke_ranking.py
+++ machine-learning-client/joke_ranking.py
@@ -15,12 +15,16 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
+
 class OutOfRangeError(Exception):
-    """Custom exception for the cases if Google Ai returns an invalid score"""
+    """Custom exception for the cases if Google AI returns an invalid score"""
+
     def __str__(self):
         return "Invalid Score was generated"
 
-#JOKE = "They done messed up & selected me for jusry duty, SOMEBODY COMING HOME"
+
+# JOKE = "They done messed up & selected me for jusry duty, SOMEBODY COMING HOME"
+
 
 def generate_score(joke):
     """Given a string(joke) generates a score 1-100 based on how funny it is"""
@@ -29,8 +33,9 @@ def generate_score(joke):
         model="gemini-3-flash-preview",
         config=types.GenerateContentConfig(
             system_instruction="""Treat the input as a joke.
-              Return only a score 1 to 100 of the joke based on how funny it is"""),
-        contents=f"{joke}"
+              Return only a score 1 to 100 of the joke based on how funny it is"""
+        ),
+        contents=f"{joke}",
     )
 
     score = "".join(filter(str.isdigit, response.text))
