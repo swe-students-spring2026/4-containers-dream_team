64085f04faa564ee8f4f5acdeb8dae0cd5208fb8
Landreani04 <Lda8263@nyu.edu>
2026-04-15 15:08:05 -0400

    fixed frontend app.py. added backend app.py

--- machine-learning-client/voice_to_text.py
+++ machine-learning-client/voice_to_text.py
@@ -1,25 +1,10 @@
 """File to take web apps voice files and convert it to text"""
 
 import os
-from flask import Flask, request
 import assemblyai as aai
 
-app = Flask(__name__)
 aai.settings.api_key = os.getenv("VTT_API_KEY")
 
-"""Helper function to retrieve audio"""
-
-
-def get_audio():
-    if "joke" not in request.files:
-        raise FileNotFoundError("No jokes have been uploaded")
-    audio = request.files["joke"]
-    if audio.filename == "":
-        raise FileNotFoundError("Joke has not been uploaded")
-    return audio
-
-
-@app.post("/")
 def voice_to_text(audio):
     """Transcribe jokes passed"""
     # takes the joke audio, and uploads to assemblyAI, also translates to english when necessary
